8dc2522da4c25b9f3148a34d909c800f8c3d7b55
dudejrtjdrp <dudejrtjdrp@naver.com>
2025-10-30 15:33:20 +0900

    feat(model): Lotto 클래스 구현
    - 로또 번호 유효성 검사 메서드 추가
    - 중복, 범위, 개수, 오름차순 검증 포함
    - getNumbers()로 복사본 반환

--- src/Model/Lotto.js
+++ src/Model/Lotto.js
@@ -1,14 +1,45 @@
+import { LOTTO_ERROR } from '../Util/constants';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers];
   }
 
-  #validate(numbers) {}
+  #validate(numbers) {
+    this.#validateArray(numbers);
+    this.#validateLength(numbers);
+    this.#validateRedundant(numbers);
+    this.#validateRange(numbers);
+    this.#validateSorted(numbers);
+  }
 
-  // TODO: 추가 기능 구현
+  #validateArray(numbers) {
+    if (!Array.isArray(numbers)) throw new Error(LOTTO_ERROR.ARRAY);
+  }
+
+  #validateLength(numbers) {
+    if (numbers.length !== 6) throw new Error(LOTTO_ERROR.COUNT_SIX);
+  }
+
+  #validateRedundant(numbers) {
+    if (new Set(numbers).size !== numbers.length) throw new Error(LOTTO_ERROR.REDUNDANT);
+  }
+
+  #validateRange(numbers) {
+    if (numbers.some((n) => n < 1 || n > 45)) throw new Error(LOTTO_ERROR.NUMBER_RANGE);
+  }
+
+  #validateSorted(numbers) {
+    if (!numbers.every((n, i, arr) => i === 0 || arr[i - 1] <= n)) {
+      throw new Error(LOTTO_ERROR.ASCENDING);
+    }
+  }
+  getNumbers() {
+    return this.#numbers.slice(); // slice()로 복사본 반환
+  }
 }
 
 export default Lotto;
